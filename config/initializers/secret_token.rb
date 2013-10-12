c3aa76c86579aa3c60a6e33924dd7dcc412812c1
Nikkihj <dogs@dogsoutandabout.com.au>
2013-10-12 16:14:44 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dog1::Application.config.secret_key_base = 'f47e0b2bdddc65600fa16144ba851b94016ac1e8adb73716c60e81e6e507ab3bcc3231438db9d5f035e663ca49ac951d60227921d02e87e1c8947004a775c07c'
